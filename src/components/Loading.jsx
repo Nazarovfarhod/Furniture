627091714bc4f5fed71f53e7003c6e9d11819ff3
farxodnazarov <farhodnazarov002gmail.com>
2024-08-18 14:10:51 +0500

    changed loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Loading() {
   return (
     <div className="h-screen flex  justify-center items-center ">
-      <span className="loading loading-dots loading-lg"></span>
+      <span className="loading loading-infinity loading-lg"></span>
     </div>
   );
 }
